9e1dd7cecef649c742244a344bf0910fd0c0f4d0
Leivenwong <13852904@qq.com>
2018-09-09 22:26:40 +0800

    release data_out_1.1

--- data_out.py
+++ data_out.py
@@ -4,33 +4,26 @@ import numpy as np
 from settings import Settings
 from data_functions import if_fuc
 
-
-
 ai_settings = Settings()
 
 print(ai_settings.business_type)
 print(ai_settings.distribution_column)
 print(ai_settings.staff_exceptions)
 
-#read excel file input data
-#deposit_detail = pd.read_excel('abc.xlsx',
-    #'Sheet1',converters={'业务子类型': str})
+#read row data and dataclean
 deposit_detail = pd.read_excel('deposit_detail.xlsx', 'Sheet1',
     converters = {'业务子类型': str, '客户代码2': str})
 deposit_detail = pd.DataFrame(deposit_detail)
 deposit_detail = deposit_detail.loc[deposit_detail['业务子类型'].isin(
     ai_settings.business_type)]
 
+#delete rows where customer name not in settings
 criterion = lambda row: row['员工名称'] not in ai_settings.staff_exceptions
-#not_in = df[df.apply(criterion, axis=1)]
 deposit_detail = deposit_detail[deposit_detail.apply(criterion,
     axis='columns')]
-#print(deposit_detail)
 
-
-#select column for compute
+#select column for compute according to settings
 selected_dataframe = deposit_detail[ai_settings.distribution_column]
-#print(selected_dataframe)
 
 #group by customer id
 group_2 = selected_dataframe.loc[:,['客户代码2','分成后季日均']]
@@ -45,18 +38,12 @@ group_3 = selected_dataframe.loc[:,['客户代码2','余额分成比率']]
 group_3 = group_3.groupby('客户代码2').sum()
 grouped_data = pd.merge(grouped_data, group_3, on='客户代码2', how='left')
 
-#compute aplly function
+#aplly if condition function
 grouped_data['out'] = grouped_data.apply(lambda x: if_fuc(
     col_if_s=x.分成后季日均_y, col_if_y=x.分成后年日均_y,
     col_if_ye=x.余额分成比率_y, col_compute_s=x.分成后季日均_x,
     col_compute_y=x.分成后年日均_x, col_compute_ye=x.余额分成比率_x), axis=1)
 
-
-print(grouped_data['分成后季日均_y'])
-grouped_data.to_excel('def.xlsx','Sheet1')
-
-
-
-
-
-
+#output data to excel and print this version
+grouped_data.to_excel('data_out.xlsx','Sheet1')
+print("Release version: data_out_1.1")
--- settings.py
+++ settings.py
@@ -4,8 +4,11 @@ class Settings():
     """inicaite settings"""
     def __init__(self):
         """iniciate settings"""
+        #setting business type needed
         self.business_type = fc.read_files('business_type.txt')
+
+        #columns needed for compute
         self.distribution_column = fc.read_files('distribution_column.txt')
+
+        #setting staff exceptions
         self.staff_exceptions = fc.read_files('staff_exceptions.txt')
-
-
